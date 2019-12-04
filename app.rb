caf27a8b1775168e65c9701949726f4bc36c54af
DavydovDenys <denvanrain@gmail.com>
2019-12-04 15:08:22 +0200

    adit barbershop

--- app.rb
+++ app.rb
@@ -15,15 +15,40 @@ get '/visit' do
 	erb :visit
 end
 
-get '/contacts' do
-	erb :contacts
+get '/users' do
+	
+	erb :users
+end
+
+post '/users' do 
+	@login = params[:login]
+	@password = params[:password]
+	if @login == 'admin' and @password == 'secret'
+		erb :users_txt
+	else
+		@title = 'Oooops...'
+		@message = 'Sorry, wrong data!'
+		erb :message
+	end
 end
 
 post '/visit' do
 	@username = params[:username]
 	@phone = params[:phone]
 	@datetime = params[:date_time]
+	@barber = params[:barber]
 	users = File.open("./public/users.txt", "a")
-	users.write("\nИмя: #{@username}\nТелефон: #{@phone}\nДата и время: #{@date_time}\n")
+	users.write("\nИмя: #{@username}\nТелефон: #{@phone}\nДата и время: #{@date_time}\nМастер: #{@barber}\n")
 	users.close
+
+	@title = "Спасибо!"
+ 	@message = "Уважаемый, #{@username}, мы ждём вас #{@datetime}"
+ 	erb :message
 end
+
+get '/create' do
+   
+    erb :create
+    
+end
+
